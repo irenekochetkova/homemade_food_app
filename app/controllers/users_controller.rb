7778e38122d8d7de2a454b21c3ac95f71dbddb99
Irina <irenekochetkova@gmail.com>
2018-07-14 13:36:49 -0700

    installed  authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      image_url: params[:image_url],
+      phone_number: params[:phone_number],
+      zipcode: params[:zipcode],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      provider: params[:provider]
+      )
+
+    if user.save
+      render json: {message: "User created successfully"}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :dishes
   has_many :orders
   has_many :carted_dishes
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
