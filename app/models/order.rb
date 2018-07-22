1ca0b992098efd1be60091bee235d9380027173a
Irina <irenekochetkova@gmail.com>
2018-07-22 16:29:48 -0700

    created an object of the model order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,16 @@ class Order < ApplicationRecord
 
   has_many :carted_dishes
   has_many :dishes, through: :carted_dishes
+
+  def as_json
+    {
+      id: id,
+      user_id: user_id,
+      carted_dishes: carted_dishes.as_json,
+      subtotal: subtotal,
+      total: total,
+      tax: tax
+    }
+  end
+
 end
