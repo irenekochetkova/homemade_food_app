82ab6265dd09c77a3a39f05bba1a7b843800f21a
Irina <irenekochetkova@gmail.com>
2018-07-22 16:22:49 -0700

    created object of carted_dish

--- app/models/carted_dish.rb
+++ app/models/carted_dish.rb
@@ -2,4 +2,20 @@ class CartedDish < ApplicationRecord
   belongs_to :dish
   belongs_to :user
   belongs_to :order, optional: true
+
+  def subtotal_carted
+    dish.price * quantity
+  end
+
+  def as_json
+    {
+      id: id,
+      quantity: quantity,
+      status: status,
+      user: user,
+      subtotal_carted: subtotal_carted,
+      dish: dish.as_json
+    }
+  end
+
 end
