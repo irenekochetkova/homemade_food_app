2f539592581ef7e9698b63acc0d0e6b40632071e
Irina <irenekochetkova@gmail.com>
2018-07-22 16:49:07 -0700

    created routes for orders_controller, carted_dish_controllers and dish_controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,26 @@ Rails.application.routes.draw do
 
   get "/categories" => "categories#index"
 
-  post '/user_token' => 'user_token#create'
-  
+  post '/user_token' => 'user_token#create'  
   get "/current_user" => "users#show"
-
   post "/users" => "users#create"
   patch "/current_user" => "users#update"
-  delete "/current_user" => "users#destroy"
-
-  
+  delete "/current_user" => "users#destroy"  
 
   get "/dishes" => "dishes#index"
   post "/dishes" => "dishes#create"
   patch "/dishes/:id" => "dishes#update"
+  patch "/dishes/:id" => "dishes#update"
+  delete "/dishes/:id" => "dishes#destroy"
 
+  get "/orders" => "orders#index"
+  post "/orders" => "orders#create"
+  delete "/orders/:id" => "orders#destroy"
+
+  get "/carted_dishes" => "carted_dishes#index" 
+  post "/carted_dishes" => "carted_dishes#create" 
+  patch "/carted_dishes/:id" => "carted_dishes#update" 
+  delete "/carted_dishes/:id" => "carted_dishes#destroy" 
 
  
 end
